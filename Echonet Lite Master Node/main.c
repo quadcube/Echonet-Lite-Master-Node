8e26eda697a09f50b90dbaa4cbe40706d7555659
James Ooi <quad_cube@hotmail.com>
2016-07-04 16:18:52 +0900

    Initial Commit

---
+++ Echonet Lite Master Node/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Echonet Lite Master Node
+//
+//  Created by QuadCube on 04/07/2016.
+//  Copyright © 2016 QuadCube. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
