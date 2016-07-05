f57a435bd0b5077c921b26e6fc0ab73782ef6a99
James Ooi <quad_cube@hotmail.com>
2016-07-05 11:17:26 +0900

    Added Echonet Lite prototype functions

--- Echonet Lite Master Node/main.c
+++ Echonet Lite Master Node/main.c
@@ -7,9 +7,45 @@
 //
 
 #include <stdio.h>
+#include <ctype.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <string.h>
+#include <stdlib.h>
+#include "network.h"
+#include "error.h"
+#include "echonet_lite.h"
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+int main(int argc, const char * argv[])
+{
+    //int i;
+    char buffer[DEFAULT_BUFFER_SIZE];
+    unsigned long packet_length;
+    init_network("127.0.0.1");
+    echonet_setEHD2(1);
+    echonet_setTID(0xABCD);
+    echonet_printpacket(10);
+    echonet_clearpacket_data();
+    echonet_printpacket(10);
+    while(1)
+    {
+        printf("Type a sentence to send to server:\n");
+        fgets(buffer,DEFAULT_BUFFER_SIZE,stdin);
+        packet_length=3;
+        send_udp(buffer,&packet_length);
+        memset(buffer, 0, sizeof(buffer));
+        recv_udp(buffer,&packet_length);
+        
+        //printf("Received from server: %s\n",buffer);
+        printf("Packet size: %lu\n",packet_length);
+        printf("Received: %s",buffer);
+        /*for(i=0;i<(packet_length);i++)
+        {
+            printf("%c",buffer[i]);
+        }*/
+        printf("!");
+    }
+    
     return 0;
 }
